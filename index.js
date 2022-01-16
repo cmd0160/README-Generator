cd137f207c0a02957a3e6d92ea95e4aa3ca53970
cdtest <corydavis1989@gmail.com>
2022-01-16 11:31:47 -0600

    added fs and inquirer require statements/generated first prompt

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 // TODO: Include packages needed for this application
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+[
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the Project title?',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please enter your project title!');
+        return false;
+      }
+    }
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
 
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-  };
-  
-  printProfileData(profileDataArgs);
+
 
 
 
